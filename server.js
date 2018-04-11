c75982bacc3d910f9e9c3b12479e11f32c7779a8
AshishkrGoyal <15103140ashish@gmail.com>
2018-04-11 09:21:33 +0530

    local-ip-setup

--- server.js
+++ server.js
@@ -3,12 +3,28 @@ const request = require('request');
 const yargs = require('yargs');
 
 const port = 8888||process.env.port;
+const ip = '172.16.96.246'; 
 const app = express();
 
 app.get('/',function (req, res) {
-    res.send("At root route/");
+    res.send("welcome to the route section for e-learning app of jaypee noida :)");
 });
 
-app.listen(port,function () {
-    console.log("App is running on port"+port);
+
+//creating A dynamic route for mentor section
+app.get('/:Mentor',function (req, res) {
+    res.send(req.params.Mentor);
+})
+
+
+
+app.listen(port,ip,function (err) {
+    if(err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        console.log("app is running on local ip");
+    }
 });
