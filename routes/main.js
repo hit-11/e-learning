c43083a200022129fb28680f2c6443b1a0a9234b
ashishkrgoyal <15103140ashish@gmail.com>
2018-04-17 16:00:20 +0530

    post-req-setup

--- routes/main.js
+++ routes/main.js
@@ -4,6 +4,11 @@ module.exports = function(app){
     app.get('/',function(req, res, next)
     {
         res.json("Ashishkr");
+    });
+
+    app.get('/local',function(req, res, next)
+    {
+        res.render('index');
     })
 };
 /*const express = require('express');
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
+const ejs = require('ejs');
+const engine = require('ejs-mate');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+
 
 const secret = require('./config/secret');
 
@@ -22,8 +28,20 @@ mongoose.connect(secret.db,function(err)
 
 const app = express();
 
+app.use(express.static(__dirname+'/public'))
+app.set('view engine','ejs');
+app.engine('ejs',engine);
+
+app.set('views',__dirname+'/views')
+
 app.use(helmet());
 app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cookieParser());
+
 
 //require('./routes/main')(app);
 require('./routes/main')(app);
