a7f22328d972aae009b9e52b09eef50118ebc50c
AshishkrGoyal <15103140ashish@gmail.com>
2018-04-19 22:57:18 +0530

    email-null-error-solved

--- models/users.js
+++ models/users.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema(
   {
     email: {
       type: String,
-      unique: true,
+      //unique: true,
       lowercase: true,
       default: ''
       //required: true
