61a37f76c62952e910dd484dab3e3832cee1cfaa
AshishkrGoyal <15103140ashish@gmail.com>
2018-04-26 22:40:35 +0530

    login-signup-final-commit

--- routes/users.js
+++ routes/users.js
@@ -23,8 +23,10 @@ module.exports = function (app) {
   app.get('/signup',(req,res,next)=>{
     res.render('./accounts/signup');
   });
+
+
   app.get('/logout', function (req, res, next) {
-    req.logOut();
+    req.logout();
     res.redirect('/login');
   })
 
