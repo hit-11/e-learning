21a392c7996505076eaba013d83c36f61f2c0f9f
hit-11 <hiteshpeswani1111@gmail.com>
2018-04-28 22:27:22 +0530

    About us page added

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,8 @@
 const  express = require('express');
 const app = express();
+const fs = require('fs');
+const data = require('../data');
+
 module.exports = function(app){
     app.get('/',function(req, res, next)
     {
@@ -10,10 +13,13 @@ module.exports = function(app){
     {
         res.render('main/home');
     })
-    /*app.get('/about',(req,res,next)=>{
-        res.render('main/about');
+    app.get('/about',(req,res,next)=>{
+        var pageno = req.query.page;
+        if(pageno === undefined || pageno> 3)
+            pageno=0;
+        res.render('main/about',{pageno: pageno, name:data.name , profile:data.profile , email:data.email  , imgsrc:data.img });
     })
-    app.get('/contact',(req,res,next)=>{
+    /*app.get('/contact',(req,res,next)=>{
         res.render('main/contact');
     })*/
 };
